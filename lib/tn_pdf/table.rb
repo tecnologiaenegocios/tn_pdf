0de1cf4da522f3cb053bc8ba62e7c1bd7c0413ba
Renato Riccieri <renato.riccieri@gmail.com>
2011-06-03 16:05:31 -0300

    experimental subtable-based implementation

--- lib/tn_pdf/table.rb
+++ lib/tn_pdf/table.rb
@@ -53,7 +53,9 @@ module TnPDF
       document.bounding_box([x_pos, document.cursor],
                             :width => prawn_table.width,
                             :height => max_height) do
-        prawn_table.draw
+        document.table([[header_table], *minitables]) do |table|
+          stylize_table(table)
+        end
       end
     end
 
@@ -82,23 +84,42 @@ module TnPDF
 
     def prawn_table
       @prawn_table ||= document.make_table([columns_headers]+rows,
-                          :width => document.bounds.width) do |table|
-        table.header = self.multipage_headers
-        table.cells.borders = [] unless self.borders
-        table.row_colors = [self.odd_row_color, self.even_row_color]
+                          :width => document.bounds.width.round) do |table|
+        stylize_table(table)
+      end
+    end
 
-        columns.each_with_index do |column, index|
-          style = column.style.reject { |k, v| [:format, :decimal].include? k }
-          table.columns(index).style(style)
+    def minitables
+      rows.map do |row|
+        minitable = document.make_table([row], :width => prawn_table.width) do |table|
+          table.column_widths = prawn_table.column_widths
+          columns.each_with_index do |column, index|
+            style = column.style.reject { |k, v| [:format, :decimal].include? k }
+            table.columns(index).style(style)
+          end
+          stylize_table(table)
         end
+        [minitable]
+      end
+    end
 
+    def header_table
+      document.make_table([columns_headers], :width => prawn_table.width) do |table|
+        table.column_widths = prawn_table.column_widths
         header_row = table.row(0)
         header_row.background_color = self.header_color
         header_row.font_style = self.header_font_style
         header_row.size = self.header_font_size
         header_row.font = self.header_font
         header_row.align = :center
+        stylize_table(table)
       end
+    end
+
+    def stylize_table(table)
+      table.header = self.multipage_headers
+      table.cells.borders = borders || []
+      table.row_colors = [self.odd_row_color, self.even_row_color]
     end
 
   end
